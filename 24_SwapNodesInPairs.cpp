201bef4d1517f3286d02f7afc5c5cc629c83d862
tianruns <tianrun@kth.se>
2025-05-05 20:43:55 +0200

    Use virtual head node method

--- 24_SwapNodesInPairs.cpp
+++ 24_SwapNodesInPairs.cpp
@@ -7,30 +7,50 @@ struct ListNode{
 
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
+    // ListNode* swapPairs(ListNode* head) {
         
-        if (!head || !(head -> next))
-            return head;
+    //     if (!head || !(head -> next))
+    //         return head;
 
-        ListNode* curr = head;
-        head = head -> next;
+    //     ListNode* curr = head;
+    //     head = head -> next;
 
-        ListNode* temp = nullptr;
-        ListNode* next;
+    //     ListNode* temp = nullptr;
+    //     ListNode* next;
 
-        while(curr && curr -> next){
-            if(temp){
-                temp -> next = temp -> next -> next;
-            }
-            next = curr -> next;
-            curr -> next = next -> next;
-            next -> next = curr;
-            temp = curr;
-            curr = curr -> next;
+    //     while(curr && curr -> next){
+    //         if(temp){
+    //             temp -> next = temp -> next -> next;
+    //         }
+    //         next = curr -> next;
+    //         curr -> next = next -> next;
+    //         next -> next = curr;
+    //         temp = curr;
+    //         curr = curr -> next;
+    //     }
+
+    //     return head;
+
+        
+    // }
+
+    ListNode* swapPairs(ListNode* head){
+        ListNode* dummyNode = new ListNode(0);
+        dummyNode -> next = head;
+        ListNode* curr = dummyNode;
+        while(curr->next != nullptr && curr -> next -> next != nullptr){
+            ListNode* next = curr -> next;
+            ListNode* afterNext = next -> next;
+            curr -> next = afterNext;
+            next -> next = afterNext -> next;
+            afterNext -> next = next;
+            curr = curr -> next ->next;
         }
+        ListNode* result = dummyNode -> next;
+        delete dummyNode;
+        return result;
 
-        return head;
-
-        
     }
+
+
 };
